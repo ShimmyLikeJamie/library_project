fb92fac9ad0641abed41ce8aedd18a102c2dfb48
ShimmyLikeJamie <oppel.jamie@gmail.com>
2021-07-03 21:01:09 -0700

    Added extremely basic form w/ validation as stipulated by https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/forms

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ function addBookToLibrary(book) {
 }
 
 
-new_book_button.onclick = function() {
+/*new_book_button.onclick = function() {
 
     //get details from user about book
     let title = prompt("Enter the book title")
@@ -97,37 +97,4 @@ new_book_button.onclick = function() {
         }
     }
     toggle_cell.appendChild(toggle_button)
-}
-
-//Localstorage function taken from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
-function storageAvailable(type) {
-    var storage;
-    try {
-        storage = window[type];
-        var x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            (storage && storage.length !== 0);
-    }
-}
-
-if (storageAvailable('localStorage')) {
-    // Yippee! We can use localStorage awesomeness
-  }
-  else {
-    // Too bad, no localStorage for us
-  }
+}*/
